53ffdd0ea04b9455d85cc2ce74945dd1118c4cd4
D1018508 <D1018508@o365.fcu.edu.tw>
2023-03-20 00:19:22 +0800

    0320 upload.

---
+++ HW2_WaitingPageUpdate/main.js
@@ -0,0 +1,12 @@
+function goWebsite() {
+  window.open("https://maplestory.beanfun.com/main").focus();
+}
+function goFB() {
+  window.open("https://www.facebook.com/www.maplestory.msfans.com.tw").focus();
+}
+function goBF() {
+  window.open("https://www.beanfun.com/").focus();
+}
+function goIG() {
+  window.open("https://www.instagram.com/maplestory_tw/").focus();
+}
