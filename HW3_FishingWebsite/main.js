e30d65247816db5a8991741e8a08518552c76d39
D1018508 <D1018508@o365.fcu.edu.tw>
2023-04-23 17:50:24 +0800

    HW3 04/23 update.

---
+++ HW3_FishingWebsite/main.js
@@ -0,0 +1,14 @@
+function forget() {
+  alert("去問神奇海螺");
+}
+
+function logo() {
+  window.open("https://www.leagueoflegends.com/zh-tw/").focus();
+}
+
+function login() {
+  window.open("https://www.youtube.com/watch?v=dMTy6C4UiQ4").focus();
+}
+function register() {
+  window.open("https://signup.leagueoflegends.com/zh-tw/signup/index#/").focus();
+}r
